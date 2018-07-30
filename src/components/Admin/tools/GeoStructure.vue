657d9426038c17fbe45861c66d11b24de96e7b68
vv-a2007 <vv-a2007@yandex.ru>
2018-07-30 18:59:07 +0300

    geostr start 2

--- src/components/Admin/tools/GeoStructure.vue
+++ src/components/Admin/tools/GeoStructure.vue
@@ -17,7 +17,7 @@
                       <v-list-tile
                               v-for="geoType in geoTypes"
                               :key="geoType.id"
-                              @click="selectGeo(geoType.key)"
+                              @click="selectGeo(geoType.id)"
                       >
                           <v-list-tile-content>
                               <v-list-tile-title>{{ geoType.name }}</v-list-tile-title>
@@ -47,15 +47,74 @@
                 </v-card>
             </v-flex>
               <v-spacer></v-spacer>
-            <v-flex xs8 justify-center wrap>
+            <v-flex xs6 justify-center wrap v-show="this.curGeoId">
                 <v-card >
                     <v-card-text>
-                        <h3>Item children</h3>
+                        <h3>Default Geo children</h3>
+                        <v-flex xs12 d-flex wrap justify-center >
+                            <v-select
+                                    :items="geoTypes"
+                                    item-value="id"
+                                    item-text="name"
+                                    box
+                                    v-model="defaultChildId"
+                                    @change="setDefChild"
+                            ></v-select>
+                        </v-flex>
+
+
+                    <h3>Custom Geo Children</h3>
+                    <v-list >
+                        <v-list-tile
+                                v-for="child in customChildren"
+                                :key="child.id"
+                                @click=""
+                        >
+                            <v-list-tile-content>
+                                <v-list-tile-title>{{ child.name }}</v-list-tile-title>
+                            </v-list-tile-content>
+
+                            <v-list-tile-action>
+                                <v-btn icon ripple>
+                                    <v-icon color="grey lighten-1"  @click="delChild(child.id)">backspace</v-icon>
+                                </v-btn>
+                            </v-list-tile-action>
+                        </v-list-tile>
+                    </v-list>
+
+                        <v-form v-model="valid" ref="form" lazy-validation>
+                        <v-layout row>
+                            <v-flex xs12 wrap justify-center >
+                                <h4>You can add some custom choise</h4>
+                                <v-select
+                                        :items="curGeoValues"
+                                        item-value="id"
+                                        item-text="name"
+                                        box
+                                        label="For custom value"
+                                        @change="curCustValue = value"
+                                ></v-select>
+                                <v-spacer></v-spacer>
+                                <v-select ml3
+                                        :items="geoTypes"
+                                        item-value="id"
+                                        item-text="name"
+                                        box
+                                        label="Custom child"
+                                        @change="newCustChildId = value"
+                                ></v-select>
+
+                                <v-spacer></v-spacer>
+                                <v-btn ml3 class="success" @click="addCustChild">Add custom</v-btn>
+                            </v-flex>
+                        </v-layout>
+                    </v-form>
+
                     </v-card-text>
 
                 </v-card>
               </v-flex>
-
+            <v-spacer></v-spacer>
         </v-layout>
     </v-container>
 </template>
@@ -67,6 +126,13 @@ export default {
         data () {
             return {
                newGeo:"",
+               newCustChildId:null,
+               curGeoId:null,
+               curGeoValues:[],
+               curCustValue:null,
+               defaultChildId:null,
+               defaultChildName:'',
+               customChildren:[],
                valid: false,
                geoRules: [
                     v => !!v || 'Name is required'
@@ -77,21 +143,35 @@ export default {
             return this.$store.dispatch('loadGeoTypes')
         },
         computed : {
-            geoTypes () { return this.$store.getters.getGeoTypes},
-            geoItems () { return this.$store.getters.getGeoItems},
+            geoTypes () { return this.$store.getters.getGeoTypes}
         },
         methods : {
-            newGeoType() {
+            newGeoType () {
                 if (this.$refs.form.validate()) {
                     this.$store.dispatch('addGeoType', {name: this.newGeo});
                     this.newGeo = '';
                 }
             },
-            selectGeo(key) {
+            selectGeo (key) {
+                this.curGeoId = key;
+                let num = this.geoTypes.findIndex(i => i.id === key);
+                this.defaultChildId = this.geoTypes[num].defaultChildId;
+                if (this.defaultChildId !== null) {
+                    let ind = this.geoTypes.findIndex(i => i.id === this.defaultChildId);
+                    this.defaultChildName = this.geoTypes[ind].name;
+                }
+                this.customChildrenID = this.geoTypes[num].customChildrenId;
+                this.$store.dispatch('getAllValuesOfGeo', this.curGeoId);
+                this.curGeoValues = this.$store.getters.getValuesCurGeo;
+            },
+            delGeo (key) {
+                this.$store.dispatch('delGeoType', key);
 
             },
-            delGeo(key) {
-                this.$store.dispatch('delGeoType', key);
+            setDefChild () {
+                this.$store.dispatch('setDefChild', {id:this.defaultChildId, idParent:this.curGeoId})
+            },
+            addCustChild () {
 
             }
         }
--- src/store/geostructure.js
+++ src/store/geostructure.js
@@ -2,18 +2,18 @@ import * as fb from 'firebase'
 
 
 class GeoType {
-    constructor(id, name, defaultChildId = null, customChildren = null) {
+    constructor(id, name, defaultChildId = null, customChildrenId = null) {
         this.id = id;
         this.name = name;
         this.defaultChildId = defaultChildId;
-        this.customChildren = customChildren;
+        this.customChildrenId = customChildrenId;
     }
 
 }
 export default {
     state:{
         geoTypes: [],
-        geoItems: []
+        valuesCurGeo: []
     },
     mutations: {
        loadGeoTypes(state, payload) {
@@ -25,7 +25,13 @@ export default {
        delGeoType (state, payload) {
            let num = state.geoTypes.findIndex(i => i.id === payload);
            state.geoTypes.splice(num,1)
-       }
+       },
+        loadValuesCurGeo(state, payload) {
+           state.valuesCurGeo = payload;
+        },
+        setDefChild(state, payload) {
+            state.geoTypes[state.geoTypes.findIndex(i => i.id === payload.idParent)].defaultChildId=payload.id;
+        }
     },
     actions: {
         async loadGeoTypes({commit}) {
@@ -37,7 +43,7 @@ export default {
                 if (geoTypes !== null) {
                     Object.keys(geoTypes).forEach((key => {
                         const geoType = geoTypes[key];
-                        resultGeoTypes.push(new GeoType(key, geoType.name, geoType.defaultChildId, geoType.customChildren))
+                        resultGeoTypes.push(new GeoType(key, geoType.name, geoType.defaultChildId, geoType.customChildrenId))
                     }));
                     commit('loadGeoTypes', resultGeoTypes);
                     commit('setLoading', false);
@@ -81,13 +87,51 @@ export default {
                 commit('setLoading', false);
                 throw error
             }
-
-
+            },
+        async getAllValuesOfGeo({commit}, payload) {
+            let name = this.getters.getGeoTypes[this.getters.getGeoTypes.findIndex(i => i.id === payload)].name;
+            commit('clearError');
+            commit('setLoading', true);
+            const allValuesGeo = [];
+            try {
+                const fbVal = await fb.database().ref(name).once('value');
+                const geoValues = fbVal.val();
+                if (geoValues !== null) {
+                    Object.keys(geoValues).forEach((key => {
+                        const geoValue = geoValues[key];
+                        allValuesGeo.push(new geoValue(key, geoValue.name))
+                    }));
+                    commit('loadValuesCurGeo', allValuesGeo);
+                    commit('setLoading', false);
+                } else {
+                    commit('setLoading', false);
+                }
+            }
+            catch (error) {
+                commit('setError', error.message);
+                commit('setLoading', false);
+                throw error
+            }
+        },
+        async setDefChild({commit}, payload) {
+            commit('clearError');
+            commit('setLoading', true);
+            try {
+                await fb.database().ref(`geotypes/${payload.idParent}`).update({defaultChildId:payload.id});
+                commit('setDefChild',payload);
+                commit('setLoading', false);
+            }
+            catch (error) {
+                commit('setError',error.message);
+                commit('setLoading', false);
+                throw error
+            }
         }
      },
 
     getters: {
         getGeoTypes(state) { return state.geoTypes },
-        getGeoItems(state) {return state.geoItems }
-        }
+        getGeoItems(state) { return state.geoItems },
+        getValuesCurGeo (state) { return state.valuesCurGeo }
+     }
 }
